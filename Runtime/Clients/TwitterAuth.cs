e5ac384bce6534c3816e3026074aed0b3fadcbeb
NekoPunch! <549643063@qq.com>
2024-12-31 09:18:50 +0800

    add twitter auth

---
+++ Runtime/Clients/TwitterAuth.cs
@@ -0,0 +1,24 @@
+﻿// ***********************************************************************
+// 文件名：         TwitterAuth.cs
+// 创建日期：       2024/12/31
+// 功能说明：
+// 作者：           NekoPunch!
+// ***********************************************************************
+
+using Cdm.Authentication.OAuth2;
+
+namespace Cdm.Authentication.Clients
+{
+    public class TwitterAuth : AuthorizationCodeFlowWithPkce
+    {
+        public override string authorizationUrl => "https://twitter.com/i/oauth2/authorize";
+        public override string accessTokenUrl => "https://api.x.com/2/oauth2/token";
+        public string userInfoUrl => "https://api.x.com/2/users/me";
+
+        public string CodeVerifier => _codeVerifier;
+        
+        public TwitterAuth(Configuration configuration) : base(configuration)
+        {
+        }
+    }
+}
---
+++ Runtime/OAuth2/RequestTokenResponse.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using UnityEngine.Scripting;
+
+namespace Cdm.Authentication.OAuth2
+{
+    [Preserve, DataContract]
+    public class RequestTokenResponse
+    {
+        private readonly Dictionary<string, string> _parameters = new();
+
+        public bool TryGetValue(string key, out string value)
+        {
+            return _parameters.TryGetValue(key, out value);
+        }
+
+        public string ToJson() => JsonConvert.SerializeObject(_parameters);
+
+        public void SetResponse(string response, string url)
+        {
+            _parameters.Clear();
+            SplitParameters(response.Substring(url.Length + 1));
+        }
+
+        public void SetParameter(string key, string value) => _parameters[key] = value;
+        
+        private void SplitParameters(string response)
+        {
+            var split = response.Split('&');
+            foreach (var kvp in split)
+            {
+                var kp = kvp.Split('=');
+                if (kp.Length > 1)
+                {
+                    _parameters[kp[0]] = kp[1];
+                }
+                else if (kp.Length > 0)
+                {
+                    _parameters[kp[0]] = null;
+                }
+            }
+        }
+    }
+}
